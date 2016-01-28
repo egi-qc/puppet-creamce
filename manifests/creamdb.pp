02adf232173f4fdca583fff4353cbdbff6ecc3a3
Paolo Andreetto <paolo.andreetto@pd.infn.it>
2016-01-28 14:57:15 +0100

    New flow for database setup

--- manifests/creamdb.pp
+++ manifests/creamdb.pp
@@ -14,6 +14,9 @@ class creamce::creamdb inherits creamce::params {
     $access_pattern = "${cream_db_host}"
   }
   
+  $enc_std_password = mysql_password("${cream_db_password}")
+  $enc_min_password = mysql_password("${cream_db_minpriv_password}")
+  
   class { 'mysql::server':
     root_password => $mysql_password,
     override_options => $override_options
@@ -21,48 +24,110 @@ class creamce::creamdb inherits creamce::params {
   
   class {'creamce::database::configure_files':}
   
-  mysql::db { "${cream_db_user}@${access_pattern}/${cream_db_name}":
-    ensure => 'present',
-    user => "${cream_db_user}",
-    password => "${cream_db_password}",
-    dbname => "${cream_db_name}",
-    charset => 'utf8',
-    host => "${access_pattern}",
-    grant => ['ALL'],
-    sql => "/etc/glite-ce-cream/populate_creamdb_mysql.puppet.sql"
+  mysql_user { "${cream_db_user}@${access_pattern}":
+    ensure        => $ensure,
+    password_hash => $enc_std_password
   }
   
-  mysql::db { "${cream_db_user}@localhost/${cream_db_name}":
-    ensure => 'present',
-    user => "${cream_db_user}",
-    password => "${cream_db_password}",
-    dbname => "${cream_db_name}",
-    charset => 'utf8',
-    host => "localhost",
-    grant => ['ALL'],
+  mysql_user { "${cream_db_user}@localhost":
+    ensure        => $ensure,
+    password_hash => $enc_std_password
+  }
+  
+  mysql_user { "${cream_db_minpriv_user}@${access_pattern}":
+    ensure        => $ensure,
+    password_hash => $enc_min_password
   }
 
-  mysql::db { "${cream_db_user}@${access_pattern}/${delegation_db_name}":
+  mysql_user { "${cream_db_minpriv_user}@localhost":
+    ensure        => $ensure,
+    password_hash => $enc_min_password
+  }
+  
+  mysql_database { "${cream_db_name}":
     ensure => 'present',
-    user => "${cream_db_user}",
-    password => "${cream_db_password}",
-    dbname => "${delegation_db_name}",
-    charset => 'utf8',
-    host => "${access_pattern}",
-    grant => ['ALL'],
-    sql => "/etc/glite-ce-cream/populate_delegationcreamdb.puppet.sql"
+    charset => 'utf8'
+  }
+  
+  mysql_database { "${delegation_db_name}":
+    ensure => 'present',
+    charset => 'utf8'
+  }
+  
+  exec { "creamdb-import":
+    command     => "cat /etc/glite-ce-cream/populate_creamdb_mysql.puppet.sql | mysql ${cream_db_name}",
+    logoutput   => true,
+    environment => "HOME=${::root_home}",
+    refreshonly => true,
+    path        => '/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin',
+    timeout     => 300,
   }
 
-  mysql::db { "${cream_db_user}@localhost/${delegation_db_name}":
-    ensure => 'present',
-    user => "${cream_db_user}",
-    password => "${cream_db_password}",
-    dbname => "${delegation_db_name}",
-    charset => 'utf8',
-    host => "localhost",
-    grant => ['ALL'],
+  exec { "delegationdb-import":
+    command     => "cat /etc/glite-ce-cream/populate_creamdb_mysql.puppet.sql | mysql ${delegation_db_name}",
+    logoutput   => true,
+    environment => "HOME=${::root_home}",
+    refreshonly => true,
+    path        => '/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin',
+    timeout     => 300,
+  }
+  
+  mysql_grant { "${cream_db_user}@${access_pattern}/${cream_db_name}.*":
+    privileges => 'ALL',
+    user       => "${cream_db_user}@${access_pattern}",
+    table      => "${cream_db_name}.*"
   }
 
-  Class ['mysql::server'] -> Class ['creamce::database::configure_files'] -> Mysql::Db["${cream_db_user}@${access_pattern}/${cream_db_name}", "${cream_db_user}@localhost/${cream_db_name}", "${cream_db_user}@${access_pattern}/${delegation_db_name}", "${cream_db_user}@localhost/${delegation_db_name}"]
+  mysql_grant { "${cream_db_user}@localhost/${cream_db_name}.*":
+    privileges => 'ALL',
+    user       => "${cream_db_user}@localhost",
+    table      => "${cream_db_name}.*"
+  }
+
+  mysql_grant { "${cream_db_user}@${access_pattern}/${delegation_db_name}.*":
+    privileges => 'ALL',
+    user       => "${cream_db_user}@${access_pattern}",
+    table      => "${delegation_db_name}.*"
+  }
+
+  mysql_grant { "${cream_db_user}@localhost/${delegation_db_name}.*":
+    privileges => 'ALL',
+    user       => "${cream_db_user}@localhost",
+    table      => "${delegation_db_name}.*"
+  }
+    
+  mysql_grant { "${cream_db_minpriv_user}@${access_pattern}/${cream_db_name}.db_info":
+    privileges => ['SELECT (submissionEnabled,startUpTime)'],
+    options    => ['GRANT'],
+    user       => "${cream_db_minpriv_user}@${access_pattern}",
+    table      => "${cream_db_name}.db_info"
+  }
+    
+  mysql_grant { "${cream_db_minpriv_user}@localhost/${cream_db_name}.db_info":
+    privileges => ['SELECT (submissionEnabled,startUpTime)'],
+    options    => ['GRANT'],
+    user       => "${cream_db_minpriv_user}@localhost",
+    table      => "${cream_db_name}.db_info"
+  }
+    
+  Class['mysql::server'] -> 
+  Class['creamce::database::configure_files'] ->
+  Mysql_user[ 
+    "${cream_db_user}@${access_pattern}", 
+    "${cream_db_user}@localhost",
+    "${cream_db_minpriv_user}@${access_pattern}",
+    "${cream_db_minpriv_user}@localhost"
+  ] ->
+  Mysql_database[ "${cream_db_name}", "${delegation_db_name}" ] ->
+  Mysql_grant[ 
+    "${cream_db_user}@${access_pattern}/${cream_db_name}.*",
+    "${cream_db_user}@localhost/${cream_db_name}.*",
+    "${cream_db_user}@${access_pattern}/${delegation_db_name}.*",
+    "${cream_db_user}@localhost/${delegation_db_name}.*" ] ->
+  Exec[ "creamdb-import", "delegationdb-import" ] ->
+  Mysql_grant[
+    "${cream_db_minpriv_user}@${access_pattern}/${cream_db_name}.db_info",
+    "${cream_db_minpriv_user}@localhost/${cream_db_name}.db_info"
+  ]
   
 }
