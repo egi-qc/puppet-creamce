4fd1558a17695e141fe7fed6991acbfa59f9ef05
Paolo Andreetto <paolo.andreetto@pd.infn.it>
2016-01-29 10:55:12 +0100

    Fixed collate for databases

--- manifests/database/create.pp
+++ manifests/database/create.pp
@@ -1,13 +1,15 @@
 class creamce::database::create inherits creamce::params {
 
   mysql_database { "${cream_db_name}":
-    ensure => 'present',
-    charset => 'latin1'
+    ensure  => 'present',
+    charset => 'latin1',
+    collate => 'latin1_general_ci'
   }
   
   mysql_database { "${delegation_db_name}":
-    ensure => 'present',
-    charset => 'latin1'
+    ensure  => 'present',
+    charset => 'latin1',
+    collate => 'latin1_general_ci'
   }
   
 }
