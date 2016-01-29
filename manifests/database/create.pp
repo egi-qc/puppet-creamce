08874868d39e3d24d019eaa5766368c220170209
Paolo Andreetto <paolo.andreetto@pd.infn.it>
2016-01-29 10:50:16 +0100

    Changed charset for databases

--- manifests/database/create.pp
+++ manifests/database/create.pp
@@ -2,12 +2,12 @@ class creamce::database::create inherits creamce::params {
 
   mysql_database { "${cream_db_name}":
     ensure => 'present',
-    charset => 'utf8'
+    charset => 'latin1'
   }
   
   mysql_database { "${delegation_db_name}":
     ensure => 'present',
-    charset => 'utf8'
+    charset => 'latin1'
   }
   
 }
