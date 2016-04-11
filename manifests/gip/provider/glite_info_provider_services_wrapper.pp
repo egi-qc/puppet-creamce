8ebd5aaebcbc6638cb28aaa17d955cf7a7b6cf74
Paolo Andreetto <paolo.andreetto@pd.infn.it>
2016-04-11 16:43:14 +0200

    Single manifests for more services

--- manifests/gip/provider/glite_info_provider_services_wrapper.pp
+++ manifests/gip/provider/glite_info_provider_services_wrapper.pp
@@ -1,7 +1,24 @@
-class creamce::gip::provider::glite_info_provider_service_rtepublisher_wrapper inherits creamce::params {
+class creamce::gip::provider::glite_info_provider_services_wrapper inherits creamce::params {
 
   package { "glite-info-provider-service":
     ensure => present,
+  }
+  
+  file { "$gippath/provider/glite-info-provider-service-cream-wrapper":
+    ensure => present,
+    owner => "root",
+    group => "root",
+    mode => 0755,
+    content => template("creamce/gip/glite-info-provider-service-cream-wrapper.erb"),
+    require => Package['glite-info-provider-service'],
+  }
+  
+  file{ "/etc/glite/info/service/glite-info-service-cream.conf":
+    ensure => present,
+    owner => "root",
+    group => "root",
+    mode => 0644,
+    content => template("creamce/glite-info-service-cream.conf.erb"),
   }
   
   file { "$gippath/provider/glite-info-provider-service-rtepublisher-wrapper":
@@ -20,5 +37,5 @@ class creamce::gip::provider::glite_info_provider_service_rtepublisher_wrapper i
     mode => 0644,
     content => template("creamce/glite-info-glue2-rtepublisher.conf.erb"),
   }
- 
+
 }
