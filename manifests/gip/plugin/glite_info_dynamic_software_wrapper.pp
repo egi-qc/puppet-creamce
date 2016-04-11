062ea8e1fdcf094824bd28ae33b2c7f5538ea27f
Paolo Andreetto <paolo.andreetto@pd.infn.it>
2016-04-11 11:54:56 +0200

    manifest for info dynamic software

--- manifests/gip/plugin/glite_info_dynamic_software_wrapper.pp
+++ manifests/gip/plugin/glite_info_dynamic_software_wrapper.pp
@@ -1,9 +1,25 @@
 class creamce::gip::plugin::glite_info_dynamic_software_wrapper inherits creamce::params {
+
+  package { "glite-ce-cream-utils":
+    ensure => present
+  }
+
   file {"$gippath/plugin/glite-info-dynamic-software-wrapper":
     ensure => present,
     owner => "root",
     group => "root",
     mode => 0755,
-    content => template("creamce/gip/glite-info-cream-glue2.erb"),
+    content => template("creamce/gip/glite-info-dynamic-software-wrapper.erb"),
+    require => Package['glite-ce-cream-utils'],
   }
+  
+  file {"$gippath/provider/glite-info-glue2-applicationenvironment-wrapper":
+    ensure => present,
+    owner => "root",
+    group => "root",
+    mode => 0755,
+    content => template("creamce/gip/glite-info-glue2-applicationenvironment-wrapper.erb"),
+    require => Package['glite-ce-cream-utils'],
+  }
+  
 }
