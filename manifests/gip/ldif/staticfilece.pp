03cd2d7a4d96a70caa7b352d0761a75432983531
Paolo Andreetto <paolo.andreetto@pd.infn.it>
2016-07-15 14:50:23 +0200

    Removed deprecated gip files

--- manifests/gip/ldif/staticfilece.pp
+++
@@ -1,9 +0,0 @@
-class creamce::gip::ldif::staticfilece inherits creamce::params {
-  file {"$gippath/ldif/static-file-CE.ldif":
-    ensure => present,
-    owner => "ldap",
-    group => "ldap",
-    mode => 0644,
-    content => template("creamce/gip/static-file-CE.ldif.erb"),
-  }  
-}
--- manifests/gip/ldif/staticfilecluster.pp
+++
@@ -1,9 +0,0 @@
-class creamce::gip::ldif::staticfilecluster inherits creamce::params {
-  file {"$gippath/ldif/static-file-Cluster.ldif":
-    ensure => present,
-    owner => "ldap",
-    group => "ldap",
-    mode => 0644,
-    content => template("creamce/gip/static-file-Cluster.ldif.erb"),
-  }  
-}
