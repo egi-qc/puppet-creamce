8e7a14bd70a2f6b3419cf7ab2f75cdc99ea9add2
Paolo Andreetto <paolo.andreetto@pd.infn.it>
2016-09-08 14:30:49 +0200

    Fixed trim in templates

--- manifests/params.pp
+++ manifests/params.pp
@@ -188,7 +188,7 @@ class creamce::params {
   $use_argus               = hiera("use_argus", true)
   $argusservice            = hiera("argusservice", undef)
   $argusport               = hiera("argusport", "8154")
-  $cream_pepc_resourceid   = hiera('cream_pepc_resourceid','http://${cream_db_host}:${ce_port}/cream')
+  $cream_pepc_resourceid   = hiera('cream_pepc_resourceid',"http://${ce_host}:${ce_port}/cream")
   $admin_list              = hiera('admin_list', [])
   $voenv                   = hiera('voenv', {})
 
