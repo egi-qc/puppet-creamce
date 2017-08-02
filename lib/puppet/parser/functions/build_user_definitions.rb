abd264e822145b28920aa6b8db8cdc995df0cb62
Paolo Andreetto <paolo.andreetto@pd.infn.it>
2017-08-02 10:11:56 +0200

    New pool account definition

--- lib/puppet/parser/functions/build_user_definitions.rb
+++ lib/puppet/parser/functions/build_user_definitions.rb
@@ -3,17 +3,17 @@ module Puppet::Parser::Functions
     voenv = args[0]
     gridmapdir = args[1]
     def_pool_size = args[2].to_i()
-    name_offset = args[3].to_i()
+    def_name_offset = args[3].to_i()
     
     result = Hash.new
     
     voenv.each do | voname, vodata |
       vodata['users'].each do | user_prefix, udata |
       
-        pool_size = udata.fetch('pool_size', def_pool_size)
         home_dir = udata.fetch('homedir', '/home')
         use_shell = udata.fetch('shell', '/bin/bash')
         name_pattern = udata.fetch('name_pattern', '%<prefix>s%03<index>d')
+        name_offset = udata.fetch('name_offset', def_name_offset)
         
         utable = udata.fetch('users_table', Hash.new)
         if utable.size > 0
@@ -28,7 +28,23 @@ module Puppet::Parser::Functions
           end
           next
         end
+        
+        uidlist = udata.fetch('uid_list', Array.new)
+        if uid_list.size > 0
+          (0...uid_list.size).each do | idx |
+            nameStr = sprintf(name_pattern % { :prefix => user_prefix, :index => (idx + name_offset) })
+            result[u_name] = {
+              'uid'        => uidlist.at(idx),
+              'groups'     => udata['groups'],
+              'gridmapdir' => "#{gridmapdir}",
+              'homedir'    => "#{home_dir}",
+              'shell'      => "#{use_shell}"
+            }
+          end
+          next
+        end
 
+        pool_size = udata.fetch('pool_size', def_pool_size)
         if pool_size > 0
         
           (0...pool_size).each do | idx |
