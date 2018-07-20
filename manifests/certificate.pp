784fb0986b6bd341206005c70d9be8ce24067d94
Pablo Orviz <orviz@ifca.unican.es>
2018-07-20 12:24:42 +0000

    remove any trace of fetch-crl

--- manifests/certificate.pp
+++ manifests/certificate.pp
@@ -2,14 +2,14 @@ class creamce::certificate inherits creamce::params {
 
   if $pki_support {
 
-    require fetchcrl
+    #require fetchcrl
     
     file { "${host_certificate}":
       ensure   => file,
       owner    => "root",
       group    => "root",
       mode     => '0644',
-      require  => Class['fetchcrl::config'],
+      #require  => Class['fetchcrl::config'],
     }
 
     file { "${host_private_key}":
@@ -17,14 +17,14 @@ class creamce::certificate inherits creamce::params {
       owner    => "root",
       group    => "root",
       mode     => '0400',
-      require  => Class['fetchcrl::config'],
+      #require  => Class['fetchcrl::config'],
     }
     
     exec { "initial_fetch_crl":
       command => "fetch-crl -l ${cacert_dir} -o ${cacert_dir} || exit 0",
       path    => "/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/local/sbin",
-      require => Class['fetchcrl::config'],
-      notify  => Class['fetchcrl::service'],
+      #require => Class['fetchcrl::config'],
+      #notify  => Class['fetchcrl::service'],
     }
 
   } else {
